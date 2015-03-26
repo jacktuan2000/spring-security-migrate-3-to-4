df6a868c98d468bf2eeea54a5b9d6739e0ab1ee7
Rob Winch <rwinch@gopivotal.com>
2015-03-26 00:28:46 -0500

    Migrate to Spring Security 4

--- jc/spring-security-3-jc/src/test/java/sample/role_/RolePrefixTests.java
+++ jc/spring-security-3-jc/src/test/java/sample/role_/RolePrefixTests.java
@@ -129,5 +129,10 @@ public class RolePrefixTests {
 		public MessageService messageService() {
 			return new HelloWorldMessageService();
 		}
+
+		@Bean
+		public static DefaultRolesPrefixPostProcessor defaultRolesPrefixPostProcessor() {
+			return new DefaultRolesPrefixPostProcessor();
+		}
 	}
 }
